cd43ae86adb0d2143d13881451b3f366b1158b44
Axel Mrak <axelmrak@gmail.com>
2025-05-07 23:33:47 -0300

    add next js

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+import "@/styles/globals.css";
+
+const urbanist = Urbanist({
+  variable: "--font-urbanist",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "TriVance App",
+  description: "Appointment scheduling app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body
+        className={`${urbanist.variable} antialiased`}
+      >
+
+        {children}
+      </body>
+    </html>
+  );
+}
--- components/icons/Logos.tsx
+++ components/icons/Logos.tsx
@@ -1,7 +1,7 @@
 export const LogoColor = (props: React.SVGProps<SVGSVGElement>) => (
   <svg
-    width={500}
-    height={500}
+    width={props.width || 500}
+    height={props.height || 500}
     viewBox="0 0 500 500"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
