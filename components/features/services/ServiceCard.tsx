9f47b9ceb545842601aea05432111be9e46ada57
Axel Mrak <axelmrak@gmail.com>
2025-05-15 17:52:09 -0300

    start with service actions

--- components/features/services/ServiceCard.tsx
+++ components/features/services/ServiceCard.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Service } from "@/types/Service";
 import Button from "@/components/ui/Button";
 import { formatInterval, formatPrice } from "@/utils/format";
@@ -12,28 +11,6 @@ export default function ServiceCard({
     service: Service
   }) {
 
-  const handleDelete = async () => {
-    const promise = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/services/${service.id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    toast.promise(promise, {
-      loading: "Eliminando servicio...",
-      success: (data: any) => {
-        if (data.status === 200) {
-          return "Servicio eliminado correctamente";
-        } else {
-          throw new Error(data.message);
-        }
-      },
-      error: (error) => {
-        return error.message;
-      },
-    });
-  }
 
   return (
     <article className="w-full flex flex-col items-start justify-between gap-4 p-4 bg-white border border-gray-300 rounded-md">
