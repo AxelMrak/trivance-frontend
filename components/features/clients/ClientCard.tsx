3d55c4d6487969b034083da8c03a047469eea6e6
Nahuposebon <nahuposebon@gmail.com>
2025-05-21 15:10:19 -0300

    style: improve button structure and formatting in ClientCard component

--- components/features/clients/ClientCard.tsx
+++ components/features/clients/ClientCard.tsx
@@ -1,7 +1,7 @@
 import { Client } from '@/types/Client';
 import Button from '@/components/ui/Button';
-import {WhatsAppIcon} from '@/components/icons/WhatsAppIcon';
-import {PhoneIcon} from '@/components/icons/PhoneIcon';
+import { WhatsAppIcon } from '@/components/icons/WhatsAppIcon';
+import { PhoneIcon } from '@/components/icons/PhoneIcon';
 
 export default function ClientCard({
   client,
@@ -17,22 +17,32 @@ export default function ClientCard({
       <header className="w-full flex items-center justify-between gap-2">
         <h2 className="text-xl font-semibold text-gray-800 truncate">{client.name}</h2>
       </header>
-      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
-        <Button
-          variant="tertiary"
-          className="w-full !text-md font-normal !text-[#25d366] border !border-[#25d366] flex items-center justify-center gap-2"
-        >
-          <WhatsAppIcon className="w-5 h-5" />
-          Mensaje
-        </Button>
 
-        <Button
-          variant="tertiary"
-          className="w-full !text-md font-normal !text-blue-600 border !border-blue-600 flex items-center justify-center gap-2"
+      <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
+        <a
+          href={`https://wa.me/${client.phone}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="w-full"
         >
-          <PhoneIcon className="w-5 h-5" />
-          Llamar
-        </Button>
+          <Button
+            variant="tertiary"
+            className="w-full !text-md font-normal !text-[#25d366] border !border-[#25d366] flex items-center justify-center gap-2"
+          >
+            <WhatsAppIcon className="w-5 h-5" />
+            Mensaje
+          </Button>
+        </a>
+
+        <a href={`tel:${client.phone}`} className="w-full">
+          <Button
+            variant="tertiary"
+            className="w-full !text-md font-normal !text-blue-600 border !border-blue-600 flex items-center justify-center gap-2"
+          >
+            <PhoneIcon className="w-5 h-5" />
+            Llamar
+          </Button>
+        </a>
 
         <Button
           variant="primary"
