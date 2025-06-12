9af69630fd510650688c15d675d3a1f4470cd649
Agustin Castillo <agustin2015.castillo@gmail.com>
2025-06-11 22:29:27 -0300

    add RoleGuard component for user role-based access control

--- types/Route.ts
+++ types/Route.ts
@@ -1,5 +1,8 @@
+import { UserRole } from "@/types/User";
+
 export type AppRoute = {
-  path: string;
-  label: string;
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+	path: string;
+	label: string;
+	icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+	minRole: UserRole;
 };
