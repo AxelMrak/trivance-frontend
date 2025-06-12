5aef63e41e6fafdfb66a10b64577c0e1e1d891dc
laujrz <lautaro.ajrz@gmail.com>
2025-06-12 10:38:51 -0300

    refactor(SidebarNav): streamline accessible routes filtering logic

--- components/layouts/sidebar/SidebarNav.tsx
+++ components/layouts/sidebar/SidebarNav.tsx
@@ -1,22 +1,21 @@
 import { routes } from '@/lib/navigation';
 import { AppRoute } from '@/types/Route';
 import SidebarNavItem from '@components/layouts/sidebar/SidebarNavItem';
-import { useUser } from '@/context/UserContext'; 
+import { useUser } from '@/context/UserContext';
 
 export default function SidebarNav({ collapsed }: { collapsed: boolean }) {
 	const { user } = useUser();
-	const accessibleRoutes = routes.filter(
-		(route) => user.user!.role >= route.minRole
-	); 
+	const accessibleRoutes =
+		user.user &&
+		!user.loading &&
+		routes.filter((route) => user.user!.role >= route.minRole);
 
 	return (
 		<nav className="flex flex-col gap-14 p-4">
-			{accessibleRoutes.map((route: AppRoute) => (
-				<SidebarNavItem 
-        key={route.path} 
-        item={route} 
-        collapsed={collapsed} />
-			))}
+			{accessibleRoutes &&
+				accessibleRoutes.map((route: AppRoute) => (
+					<SidebarNavItem key={route.path} item={route} collapsed={collapsed} />
+				))}
 		</nav>
 	);
 }
