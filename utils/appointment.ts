7869a672b97aa2e18af8d7653858fc21ab417574
Nahuposebon <nahuposebon@gmail.com>
2025-09-10 11:11:05 -0300

    update appointment form to include additional time slot and fix description formatting

--- utils/appointment.ts
+++ utils/appointment.ts
@@ -7,7 +7,7 @@ export interface FormData {
   date: string;
   time: string;
   start_date?: string;
-  description: string;
+  description: string;  
   status: AppointmentStatus;
 }
 
@@ -16,6 +16,7 @@ export const timeSlots: string[] = [
   "10:00",
   "11:00",
   "12:00",
+  "13:00",
   "14:00",
   "15:00",
   "16:00",
